cf02b7181d9ccef3f8d3913ef564690eea130579
toasterbrain28 <talonbarkie28@gmail.com>
2025-02-01 01:12:38 -0700

    second commit

--- server/src/routes/htmlRoutes.ts
+++ server/src/routes/htmlRoutes.ts
@@ -4,8 +4,10 @@ import { Router, type Request, type Response } from "express";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const router = Router();
+
 // TODO: Define route to serve index.html
 router.get("/", (_req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, "../../../client/index.html"));
 });
+
 export default router;
